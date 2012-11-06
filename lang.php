1f24f11f32cf9ac496798fa72e2624e4c31626d1
Chris Johnson <cjohnson@dosomething.org>
2012-11-06 15:42:25 -0500

    Language tweaks

--- lang.php
+++ lang.php
@@ -3,21 +3,21 @@
 $responses = array(
   'aider_no_aidees' => 'There aren\'t unattended needs of this nature reported in your area currently. Pls spread the word about this resource! Have people text SANDY to 69866 w/ needs.',
 
-  'aider_helping' => 'Thanks for taking responsibility for this.',
+  'aider_helping' => 'Thank you for taking responsibility for this task. It will be taken off the need list. Reply CANCEL if you cannot help.',
 
-  'aider_try_again' => 'Oops. Something broke. Please try again by texting MUTUALAID to 69866.',
+  'aider_try_again' => 'Sorry, we couldn\'t understand your response. Please start over by texting MUTUALAID to 69866.',
 
-  'aider_rate_limited' => 'You can only volunteer to help :limit people per :time',
+  'aider_rate_limited' => 'At this time, our system only allows you to volunteer to help :limit people per :time',
 
   'aider_response' => 'Location: :address (:phone). Reply YES to take responsibility for this address. It will be removed from the need list; only reply if you will definitely help within 24 hrs. Reply NEXT for another address instead.',
 
-  'aider_no_more_aidees' => 'There aren\'t any more needs in your area. Pls spread the word by having people text SANDY to 69866 or reply NEXT to start again.',
+  'aider_no_more_aidees' => 'You\'ve reached the end of this list. Reply NEXT to start over. Pls spread the word by having people in need text SANDY to 69866.',
 
-  'aidee_new_request' => 'Your request has been recorded. We\'ll send the next available volunteer to help you.',
+  'aidee_new_request' => 'Your request has been recorded. We will try to send the next available volunteer to help you.',
 
-  'aidee_recently_helped' => 'We\'ve got your request and someone committed to helping you. If no one shows up in the next 24 hours, text us back.',
+  'aidee_recently_helped' => 'A volunteer has said they would come help you. If no one shows up within 24 hrs, please text SANDY to 69866 again.',
 
-  'aidee_help_pending' => 'We\'ve got your request. No one has replied to help you yet.',
+  'aidee_help_pending' => 'We already have this request in the system. No one has replied to help you yet, but please be patient!',
 
   'unrecognized_keyword' => 'We don\'t recognize that keyword. Please try again.',
 );
